0b6bb21c9b18c9cfafde0253ccf80559a441f035
Shamod Lacoul <shamod@gmail.com>
2019-10-29 12:09:01 -0700

    added fonts and basic style to login and register

--- lib/login.dart
+++ lib/login.dart
@@ -6,6 +6,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class LoginPageFormState extends State<LoginPage> {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
   final _formKey = GlobalKey<FormState>();
 
   @override
@@ -19,45 +20,78 @@ class LoginPageFormState extends State<LoginPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            TextFormField(
-              decoration: InputDecoration(
-                  labelText: 'Email'
-              ),
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  labelText: 'Password'
-              ),
-              validator: (value) {
-                if (value.isEmpty) {
-                  return 'Please enter some text';
-                }
-                return null;
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: RaisedButton(
-                onPressed: () {
-                  if (_formKey.currentState.validate()) {
-                    Scaffold.of(context).showSnackBar(SnackBar(
-                      content: Text('Processing Data'),
-                    ));
-                  }
-                },
-                child: Text('Submit'),
-              ),
-            )
+            emailField,
+            passwordField,
+            _loginButton(context),
           ],
         ),
       ),
     );
   }
+
+  final emailField = Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Email',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        hintText: "Email",
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(32.0)),
+      ),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter some text';
+        }
+        return null;
+      },
+    ),
+  );
+
+  final passwordField = Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        hintText: "Password",
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(32.0)),
+      ),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter some text';
+        }
+        return null;
+      },
+    ),
+  );
+
+  _loginButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Material(
+        elevation: 5.0,
+        borderRadius: BorderRadius.circular(30.0),
+        color: Color(0xff01A0C7),
+        child: MaterialButton(
+          minWidth: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          onPressed: () {
+            if (_formKey.currentState.validate()) {
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text('Processing Data'),
+              ));
+            }
+          },
+          child: Text("Login",
+              textAlign: TextAlign.center,
+              style: style.copyWith(
+                  color: Colors.white, fontWeight: FontWeight.bold)),
+        ),
+      ),
+    );
+  }
 }
+
--- lib/register.dart
+++ lib/register.dart
@@ -1,69 +1,108 @@
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatelessWidget {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Register'),
+      appBar: AppBar(
+        title: Text('Register'),
+      ),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            nameField,
+            emailField,
+            passwordField,
+            _registerButton(context),
+          ],
         ),
-        body: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: 'Name'
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                    labelText: 'Email'
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                    labelText: 'Password'
-                ),
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: RaisedButton(
-                  onPressed: () {
-                    if (_formKey.currentState.validate()) {
-                      Scaffold.of(context).showSnackBar(SnackBar(
-                        content: Text('Processing Data'),
-                      ));
-                    }
-                  },
-                  child: Text('Submit'),
-                ),
-              )
-            ],
-          ),
+      ),
+    );
+  }
+
+  final nameField = Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Name',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        hintText: "Name",
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter some text';
+        }
+        return null;
+      },
+    ),
+  );
+
+  final emailField = Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Email',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        hintText: "Email",
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter some text';
+        }
+        return null;
+      },
+    ),
+  );
+
+  final passwordField = Padding(
+    padding: const EdgeInsets.all(16.0),
+    child: TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        hintText: "Password",
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+      validator: (value) {
+        if (value.isEmpty) {
+          return 'Please enter some text';
+        }
+        return null;
+      },
+    ),
+  );
+
+  _registerButton(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Material(
+        elevation: 5.0,
+        borderRadius: BorderRadius.circular(30.0),
+        color: Color(0xff01A0C7),
+        child: MaterialButton(
+          minWidth: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          onPressed: () {
+            if (_formKey.currentState.validate()) {
+              Scaffold.of(context).showSnackBar(SnackBar(
+                content: Text('Processing Data'),
+              ));
+            }
+          },
+          child: Text("Register",
+              textAlign: TextAlign.center,
+              style: style.copyWith(
+                  color: Colors.white, fontWeight: FontWeight.bold)),
         ),
+      ),
     );
   }
 }
