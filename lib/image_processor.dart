91da262a8cd7c0eb76c28a01571fcb5fee1bba6f
Shamod Lacoul <shamod@gmail.com>
2019-11-02 11:40:17 -0700

    sample tflite model run

--- lib/image_processor.dart
+++ lib/image_processor.dart
@@ -1,11 +1,16 @@
-
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:tflite/tflite.dart';
 
 class ImageProcessor extends StatefulWidget {
+
+
   final File _imageFile;
+
+
   ImageProcessor(this._imageFile);
 
   @override
@@ -14,16 +19,26 @@ class ImageProcessor extends StatefulWidget {
 
 class _ImageProcessorState extends State<ImageProcessor> {
   File _imageFile;
+  bool _busy = false;
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
 
   void initState() {
     super.initState();
+    _busy = true;
+
     setState(() {
       this._imageFile = widget._imageFile;
     });
+
+    _loadModel().then((val) {
+      setState(() {
+        _busy = false;
+      });
+    });
   }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Image Processor'),
@@ -36,9 +51,6 @@ class _ImageProcessorState extends State<ImageProcessor> {
             Row(
               children: <Widget>[
                 FlatButton(
-                  child: Icon(Icons.cloud_upload),
-                ),
-                FlatButton(
                   child: Icon(Icons.crop),
                   onPressed: null,
                 ),
@@ -47,10 +59,43 @@ class _ImageProcessorState extends State<ImageProcessor> {
                   onPressed: null,
                 ),
               ],
-            )
-          ]
+            ),
+            MaterialButton(
+              minWidth: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              onPressed: _predictImage,
+              child: Text(
+                "Diagnose",
+                textAlign: TextAlign.center,
+                style: style.copyWith(color: Colors.blue, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
         ],
-      )
+      ),
     );
+  }
+
+  _loadModel() async {
+    Tflite.close();
+
+    try {
+      String res = await Tflite.loadModel(
+                            model: "assets/model/model.tflite",
+                            labels: "assets/model/labels.txt"
+                          );
+      print(res);
+    } on PlatformException catch(e) {
+      print(e);
+    }
+  }
+
+  _predictImage() async {
+    if (_imageFile != null) {
+      var recognitions = await Tflite.runModelOnImage(
+          path: _imageFile.path,
+      );
+      print(recognitions);
+    }
   }
 }
