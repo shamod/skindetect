262a0cb12560e483d9ebfe173e82e2e690efd90d
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2019-11-09 15:53:39 -0800

    New slides + removed TFLite model

--- lib/image_processor.dart
+++ lib/image_processor.dart
@@ -1,15 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:tflite/tflite.dart';
 
 class ImageProcessor extends StatefulWidget {
-
-
   final File _imageFile;
-
 
   ImageProcessor(this._imageFile);
 
@@ -29,12 +24,6 @@ class _ImageProcessorState extends State<ImageProcessor> {
     setState(() {
       this._imageFile = widget._imageFile;
     });
-
-//    _loadModel().then((val) {
-//      setState(() {
-//        _busy = false;
-//      });
-//    });
   }
 
   @override
@@ -67,34 +56,13 @@ class _ImageProcessorState extends State<ImageProcessor> {
               child: Text(
                 "Diagnose",
                 textAlign: TextAlign.center,
-                style: style.copyWith(color: Colors.blue, fontWeight: FontWeight.bold),
+                style: style.copyWith(
+                    color: Colors.blue, fontWeight: FontWeight.bold),
               ),
             ),
           ],
         ],
       ),
     );
-  }
-
-  _loadModel() async {
-    Tflite.close();
-
-    try {
-      String res = await Tflite.loadModel(
-                            model: "assets/model/skinmodel.tflite",
-                          );
-      print(res);
-    } on PlatformException catch(e) {
-      print(e);
-    }
-  }
-
-  _predictImage() async {
-    if (_imageFile != null) {
-      var recognitions = await Tflite.runModelOnImage(
-          path: _imageFile.path,
-      );
-      print(recognitions);
-    }
   }
 }
