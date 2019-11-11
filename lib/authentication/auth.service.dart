39dae1847de999a9e7b277d9929033cd4fe0bf8b
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2019-11-11 12:58:08 -0800

    Types

--- lib/authentication/auth.service.dart
+++ lib/authentication/auth.service.dart
@@ -3,21 +3,24 @@ import 'package:http/http.dart' as http;
 import 'package:skindetect/authentication/user.model.dart';
 
 class AuthenticationService {
-  static const String url = 'http://192.168.0.140:5000';
-  static const Map headers = {
+  static const String authServerBaseUrl = 'http://192.168.0.140:5000';
+  static const Map<String, String> headers = {
     "Content-type": "application/json",
     "Accept": "application/json"
   };
 
-  static Future<http.Response> endpointPost(String endpoint, User user) async {
-    Future<http.Response> res;
-    String body = json.encode(user.toJson());
+  static Future<http.Response> endpointPost(String endpoint, User user) {
+    Future<http.Response> response;
+
     try {
-      res = http.post("$url/$endpoint", headers: headers, body: body);
+      String url = '$authServerBaseUrl/$endpoint';
+      String body = json.encode(user.toJson());
+
+      response = http.post(url, body: body, headers: headers);
     } catch (err) {
       print(err);
     }
 
-    return res;
+    return response;
   }
 }
